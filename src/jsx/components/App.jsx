84f8d3f946066ff423ebe05344682a37fa0c05fd
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-08 23:51:40 +0100

    add library icons normalize

--- src/jsx/components/App.jsx
+++ src/jsx/components/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
+import { DiApple } from 'react-icons/di';
 
-function App() {
+export function App() {
   return (
-    <div className="container">
-      <h1>Vite + React Template</h1>
+    <div className={styles.container}>
+      <h1>
+        <DiApple /> Vite + React Template
+      </h1>
     </div>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './jsx/components/App.jsx';
+import { App } from './jsx/components/App.jsx';
 import './css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
